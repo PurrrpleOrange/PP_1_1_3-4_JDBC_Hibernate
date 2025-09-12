b9f87319f879d86a0b54eaa7667f4c581d5302e5
Akhmat <latyev.me@gmail.com>
2025-09-12 15:30:04 +0300

    feat: createUsersTable() in dao.UserDaoJDBCImpl

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,6 +14,20 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
+        String sql = """
+                    CREATE TABLE IF NOT EXISTS users (
+                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                        name VARCHAR(255),
+                        last_name VARCHAR(255),
+                        age TINYINT);
+                    """;
+        try (Connection connection = Util.open();
+             Statement statement = connection.createStatement()) {
+            var execute = statement.execute(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
     }
 
