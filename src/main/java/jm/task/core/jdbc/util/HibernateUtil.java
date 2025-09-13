d748c83ba896ec92371fd33b667209f82e0a057a
Akhmat <latyev.me@gmail.com>
2025-09-13 12:11:21 +0300

    fixing problems (min: add sout in userservice in saveUser()

---
+++ src/main/java/jm/task/core/jdbc/util/HibernateUtil.java
@@ -0,0 +1,9 @@
+package jm.task.core.jdbc.util;
+
+import org.hibernate.SessionFactory;
+
+import static org.hibernate.boot.model.process.spi.MetadataBuildingProcess.build;
+
+public class HibernateUtil {
+//    private static final SessionFactory SessionFactory = build();
+}
